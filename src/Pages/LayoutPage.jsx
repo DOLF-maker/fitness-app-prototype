256bdbe3ae6e28ac7b1632d78a67fcfe42e03327
Dolf Yee <ruldolf.mifb@gmail.com>
2024-10-28 14:29:24 +0800

    Layout with hamburger menu

--- src/Pages/LayoutPage.jsx
+++ src/Pages/LayoutPage.jsx
@@ -1,56 +1,96 @@
-import React from 'react';
-import { Button, Container, Nav, Navbar, Dropdown } from 'react-bootstrap';
-import { Outlet, useNavigate } from 'react-router-dom';
+import React from "react";
+import { Button, Container, Nav, Navbar, Dropdown } from "react-bootstrap";
+import { Outlet, useNavigate } from "react-router-dom";
+import Offcanvas from "react-bootstrap/Offcanvas";
 
 function Layout() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleLogout = () => {
-        navigate("/");
-    };
+  const handleLogout = () => {
+    navigate("/");
+  };
 
-    const handleManageBooking = () => {
-        navigate("/myappointment"); 
-    };
-    
-    const handleProfile = () => {
-        navigate("/profile"); 
-    };
+  const handleManageBooking = () => {
+    navigate("/myappointment");
+  };
 
-    return (
-      <>
-          <Navbar className="navbar navbar-expand-md navbar-light bg-light">
-              <Container>
-                  <Navbar.Brand href="/home" className="navbar-brand h1 mt-2">Fitness Strive</Navbar.Brand>
-                  
-                  <Navbar.Toggle aria-controls="navbar-nav" />
-                  <Navbar.Collapse id="navbar-nav">
-                      <Nav className="me-auto">
-                          <Nav.Link onClick={handleProfile} style={{ cursor: 'pointer' }}>Profile</Nav.Link>
-                          <Nav.Link onClick={handleManageBooking} style={{ cursor: 'pointer' }}>Manage My Booking</Nav.Link>
-                          
-                          <Dropdown>
-                              <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-                                  Tools
-                              </Dropdown.Toggle>
+  const handleProfile = () => {
+    navigate("/profile");
+  };
 
-                              <Dropdown.Menu>
-                                  <Dropdown.Item target="_blank" href="https://www.calculatorsoup.com/calculators/health/target-heart-rate-zone-calculator.php">HR Zone</Dropdown.Item>
-                                  <Dropdown.Item target="_blank" href="https://www.calculatemacro.com/">Macros Calculator & TDEE</Dropdown.Item>
-                                  <Dropdown.Item target="_blank" href="https://www.hydrationforhealth.com/en/hydration-tools/hydration-calculator/">Hydration Calculator</Dropdown.Item>
-                              </Dropdown.Menu>
-                          </Dropdown>
-                      </Nav>
-                      <div className="md-auto">
-                          <Button variant="danger" onClick={handleLogout}>
-                              <i className="bi bi-escape"></i> Logout
-                          </Button>
-                      </div>
-                  </Navbar.Collapse>
-              </Container>
-          </Navbar>
-          <Outlet />
-      </>
+  return (
+    <>
+      {[false].map((expand) => (
+        <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+          <Container fluid>
+            <Navbar.Brand href="/home" className="navbar-brand h1 mt-2">
+              Fitness Strive
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link
+                    onClick={handleProfile}
+                    style={{ cursor: "pointer" }}
+                  >
+                    Profile
+                  </Nav.Link>
+                  <Nav.Link
+                    onClick={handleManageBooking}
+                    style={{ cursor: "pointer" }}
+                  >
+                    Manage My Booking
+                  </Nav.Link>
+
+                  <Dropdown>
+                    <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                      Fitness Tools
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                      <Dropdown.Item
+                        target="_blank"
+                        href="https://www.calculatorsoup.com/calculators/health/target-heart-rate-zone-calculator.php"
+                      >
+                        HR Zone
+                      </Dropdown.Item>
+                      <Dropdown.Item
+                        target="_blank"
+                        href="https://www.calculatemacro.com/"
+                      >
+                        Macros Calculator & TDEE
+                      </Dropdown.Item>
+                      <Dropdown.Item
+                        target="_blank"
+                        href="https://www.hydrationforhealth.com/en/hydration-tools/hydration-calculator/"
+                      >
+                        Hydration Calculator
+                      </Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                  <div className="sm-auto mt-3">
+                    <Button variant="danger" onClick={handleLogout}>
+                      <i className="bi bi-escape"></i> Logout
+                    </Button>
+                  </div>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+      <Outlet />
+    </>
   );
 }
 export default Layout;
